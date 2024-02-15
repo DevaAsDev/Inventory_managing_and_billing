319699e246bfbe5d05f93a9e819afdb6f2edbc5b
DevaAsDev <loki.downloadservice@gmail.com>
2024-02-15 23:39:38 +0530

    improved product list view

--- src/js/listLoader.js
+++ src/js/listLoader.js
@@ -18,11 +18,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Clear previous content
     itemListContainer.innerHTML = "";
+    const cName = "";
+    const cStock = null;
+    const cId = null;
 
     // Check if response has a 'data' property containing an array
     if (response && response.data && Array.isArray(response.data)) {
       // Create a list element and append items
-      const itemList = document.createElement("ul");
+      //const itemList = document.createElement("ul");
 
       response.data.forEach((item) => {
         const itemDiv = document.createElement("div");
@@ -31,22 +34,32 @@ document.addEventListener("DOMContentLoaded", function () {
           "d-flex",
           "flex-wrap",
           "flex-row",
-          "m-2",
-          "justify-content-around",
+          "justify-content-between",
           "align-content-center",
-          "p-3",
-          "bg-body-primary",
-          "m-3"
+          "p-1",
+          "bg-body-primary"
         );
 
         // Add an onClick listener to each itemDiv
         itemDiv.addEventListener("click", () => {
           // You can perform actions when an item is clicked
+          document.querySelectorAll(".item").forEach((el) => {
+            el.classList.remove("activated");
+          });
+
+          // Add "active" class to the clicked item
+          itemDiv.classList.add("activated");
+
           console.log(`Item clicked: ${JSON.stringify(item)}`);
           popWindow_edit.style.display = "grid";
           const update_item = document.getElementById("btn_add");
           const xName = document.getElementById("xname");
           const xStock = document.getElementById("xstock");
+
+          //global declaration
+          cName = item.name;
+          cStock = item.cSock;
+          cId = item.id;
 
           xName.value = item.name;
           xStock.value = item.cStock;
@@ -93,30 +106,25 @@ document.addEventListener("DOMContentLoaded", function () {
           });
         });
 
-        // Add a double-click event listener to each itemDiv
-        /*itemDiv.addEventListener("dblclick", () => {
-            // You can perform actions when an item is double-clicked
-            console.log(`Item double-clicked: ${JSON.stringify(item)}`);
-            console.log(
-              `Item double-clicked: ${JSON.stringify(item.name)}`
-            );
-          });*/
-
         // Create divs for the properties you want to display
         const nameDiv = document.createElement("div");
         nameDiv.classList.add("name");
+        nameDiv.classList.add("flex_grow");
         nameDiv.textContent = "Name : " + item.name;
 
         const itemCodeDiv = document.createElement("div");
         itemCodeDiv.classList.add("itemCode");
+        itemCodeDiv.classList.add("flex_grow");
         itemCodeDiv.textContent = "Item Code : " + item.itemCode;
 
         const cStockDiv = document.createElement("div");
         cStockDiv.classList.add("cStock");
+        cStockDiv.classList.add("flex_grow");
         cStockDiv.textContent = "Stock : " + item.cStock;
 
         const skuDiv = document.createElement("div");
         skuDiv.classList.add("sku");
+        skuDiv.classList.add("flex_grow");
         skuDiv.textContent = "SKU : " + item.sku;
 
         // Append the property divs to the item div
