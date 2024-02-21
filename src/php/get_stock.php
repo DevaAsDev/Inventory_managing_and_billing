9696864d9f68f6a87055a06a5f88013936db55be
DevaAsDev <loki.downloadservice@gmail.com>
2024-02-21 08:00:53 +0530

    improved data fetching api

--- src/php/get_stock.php
+++ src/php/get_stock.php
@@ -15,7 +15,7 @@ if (!$conn) {
     $response = ['error' => 'Unable to connect to the database'];
 } else {
     // Fetch data from the items table
-    $itemsQuery = "SELECT id, name ,itemCode sku FROM items";
+    $itemsQuery = "SELECT id, name ,itemCode, sku FROM items";
     $itemsResult = mysqli_query($conn, $itemsQuery);
     $itemsData = mysqli_fetch_all($itemsResult, MYSQLI_ASSOC);
 
@@ -40,35 +40,5 @@ if (!$conn) {
     // Return the combined data as JSON response
     echo json_encode(['data' => $combinedData], JSON_UNESCAPED_UNICODE);
 }
-
-/*
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-
-$server = 'localhost';
-$username = 'id21857504_inventorymanager';
-$password = 'Inventory1234@';
-$dbname = 'id21857504_inventory';
-
-$conn = mysqli_connect($server, $username, $password, $dbname);
-
-if (!$conn) {
-    $response = ['error' => 'Unable to connect to the database'];
-} else {
-
-    $query = "SELECT * FROM stock";
-    $result = mysqli_query($conn, $query);
-
-    if ($result) {
-        $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        $response = ['data' => $data];
-    } else {
-        $response = ['error' => 'Error retrieving data from the database'];
-    }
-}
-
-echo json_encode($response);*/
 exit
     ?>
